a29abac4414a73ea4046f1d3af80b69d5eda3be9
ch-mcl <mphchmcl@gmail.com>
2025-11-12 05:40:06 +0900

    no message

---
+++ SegaRallyRevoTool/SegaRallyRevoTool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace SegaRallyRevoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            String path = "";
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+
+
+
+        }
+    }
+}
